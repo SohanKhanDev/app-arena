d0f2d94271b12d9ac8d3aa0d6ead5da6f276a7af
SohanKhanDev <sohankhandev@gmail.com>
2025-10-08 21:45:01 +0600

    statistics resp  done

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -2,30 +2,40 @@ import React from "react";
 
 const Statistics = () => {
   return (
-    <div className="interFont bg-gradient-to-br from-[#632EE3] to-[#9F62F2] flex flex-col items-center py-20">
-      <div className=" mb-10">
-        <h1 className="text-white font-bold text-5xl">
+    <div className="interFont bg-gradient-to-br from-[#632EE3] to-[#9F62F2] flex flex-col items-center py-8 sm:py-20 px-4 sm:px-6 lg:px-20">
+      <div className="md:mb-10 text-center">
+        <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl">
           Trusted by Millions, Built for You
         </h1>
       </div>
 
-      <div className="flex gap-6">
-        <div className="text-white text-center px-10">
-          <p className="opacity-80">Total Downloads</p>
-          <h1 className="font-extrabold text-[64px]">29.6M</h1>
-          <p className="opacity-80">21% more than last month</p>
+      <div className="flex flex-col sm:flex-row md:gap-6 w-full justify-center items-center">
+        <div className="text-white text-center px-6 py-4 sm:px-10 sm:py-6">
+          <p className="opacity-80 text-sm sm:text-base">Total Downloads</p>
+          <h1 className="font-extrabold text-4xl sm:text-5xl md:text-[64px]">
+            29.6M
+          </h1>
+          <p className="opacity-80 text-sm sm:text-base">
+            21% more than last month
+          </p>
         </div>
 
-        <div className="text-white text-center px-10">
-          <p className="opacity-80">Total Reviews</p>
-          <h1 className="font-extrabold text-[64px]">906K</h1>
-          <p className="opacity-80">46% more than last month</p>
+        <div className="text-white text-center px-6 py-4 sm:px-10 sm:py-6">
+          <p className="opacity-80 text-sm sm:text-base">Total Reviews</p>
+          <h1 className="font-extrabold text-4xl sm:text-5xl md:text-[64px]">
+            906K
+          </h1>
+          <p className="opacity-80 text-sm sm:text-base">
+            46% more than last month
+          </p>
         </div>
 
-        <div className="text-white text-center px-10">
-          <p className="opacity-80">Active Apps</p>
-          <h1 className="font-extrabold text-[64px]">132+</h1>
-          <p className="opacity-80">31 more will Launch</p>
+        <div className="text-white text-center px-6 py-4 sm:px-10 sm:py-6">
+          <p className="opacity-80 text-sm sm:text-base">Active Apps</p>
+          <h1 className="font-extrabold text-4xl sm:text-5xl md:text-[64px]">
+            132+
+          </h1>
+          <p className="opacity-80 text-sm sm:text-base">31 more will Launch</p>
         </div>
       </div>
     </div>
